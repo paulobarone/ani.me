104e396100565ad71a807673390d1f28e285bd2c
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-03-04 15:26:12 -0300

    feat: add header
    Co-authored-by: Kim <darrow12@protonmail.com>

---
+++ db.js
@@ -0,0 +1,62 @@
+[
+  {
+    "id": 1,
+    "titulo": "Nanatsu no Taizai",
+    "episode": "11"
+  },
+  {
+    "id": 2,
+    "titulo": "Darling in the Franxx",
+    "episode": "1"
+  },
+  {
+    "id": 3,
+    "titulo": "Douluo Dalu",
+    "episode": "214"
+  },
+  {
+    "id": 4,
+    "titulo": "Overlord 4",
+    "episode": "5"
+  },
+  {
+    "id": 5,
+    "titulo": "One Piece",
+    "episode": "1029"
+  },
+  {
+    "id": 6,
+    "titulo": "Fuuto Tantei",
+    "episode": "2"
+  },
+  {
+    "id": 7,
+    "titulo": "Hoshi no Samidare",
+    "episode": "6"
+  },
+  {
+    "id": 8,
+    "titulo": "Bucchigire!",
+    "episode": "6"
+  },
+  {
+    "id": 9,
+    "titulo": "Aoashi",
+    "episode": "16"
+  },
+  {
+    "id": 10,
+    "titulo": "Shadows House 2",
+    "episode": "2"
+  },
+  {
+    "id": 11,
+    "titulo": "Summer Time Rendering",
+    "episode": "19"
+  },
+  {
+    "id": 12,
+    "titulo": "Orient 2",
+    "episode": "2"
+  },
+]
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import styles from './Header.module.scss';
+import menu from './menu.svg';
+import search from './search.svg';
+
+function Header() {
+  const navItems = ['Início', 'Lista', 'Gêneros', 'Novos episódios'];
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.logo}>Ani.me</h1>
+      <ul>
+        {navItems.map((item, key) => {
+          return <li key={key}>{item}</li>
+        })}
+      </ul>
+      <input placeholder="Buscar" />
+      <div className={styles.button_container}>
+        <img src={search} alt="Botão de pesquisa" className="search" />
+        <img src={menu} alt="Menu" className="menu" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
