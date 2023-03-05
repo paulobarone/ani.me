37623e47418ca1749c6c67036a24d1516514411a
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-03-05 15:59:52 -0300

    fix: key

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import styles from './Card.module.scss';
 
 function Card(props) {
   return (
-    <li key={props.key} className={styles.cardContainer}>
+    <li className={styles.cardContainer}>
       <div className={styles.card} style={{ backgroundImage: `linear-gradient(180deg, rgba(6, 9, 15, 0) 0%, #06090F 100%), url('${props.cape}')` }}><p>Episódio {props.episode}</p></div>
       <p>{props.title}</p>
     </li>
