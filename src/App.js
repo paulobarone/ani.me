c02e26f420d58be8e1f0dc49d93655a49bd28296
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-03-05 17:43:59 -0300

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "components/Header/Header";
 import Banner from "components/Banner/Banner";
 import Update from "components/Update/Update";
 import Recent from 'components/Recent/Recent';
+import Footer from 'components/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Update />
       <Recent />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Todos os direitos reservados aos criadores e produtores dos animes</p>
+    </footer>
+  );
+}
+
+export default Footer;
