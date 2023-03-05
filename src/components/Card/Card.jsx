17f5d2e327ba0b33a35121b937abf452ff02ee65
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-03-05 00:06:42 -0300

    :tada: feat: add update section
    Co-authored-by: Kim <darrow12@protonmail.com>

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,12 @@
 import styles from './Card.module.scss';
+import img from './img.png';
 
-function Card() {
+function Card(props) {
   return (
-    <div className={styles.cardContainer}>
-      <div className={styles.card}><p>Episódio 24</p></div>
-      <p>Nanatsu no Taizai</p>
-    </div>
+    <li className={styles.cardContainer}>
+      <div className={styles.card} style={{ backgroundImage: `linear-gradient(180deg, rgba(6, 9, 15, 0) 0%, #06090F 100%), url('${props.cape}')` }}><p>Episódio {props.episode}</p></div>
+      <p>{props.title}</p>
+    </li>
   );
 }
 
