2e9da0478119a910c7f2a53a8d975d2ee5fb68e3
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-03-07 06:08:14 -0300

    add recent cards

--- src/components/Recent/Recent.jsx
+++ src/components/Recent/Recent.jsx
@@ -1,11 +1,27 @@
 import Card from 'components/Card/Card';
 import styles from './Recent.module.scss';
+import { useContext } from 'react';
+import MyContext from 'contexts/MyContext';
+import filteredData from 'utils/filteredData';
 
 function Recent() {
+  const { data, isFetching } = useContext(MyContext);
+  const dataFiltered = !isFetching && filteredData(data, 'recent');
+
   return (
     <section className={styles.recentContainer}>
       <h3>Animes recentes</h3>
       <ul>
+        { isFetching && <p>Carregando...</p> }
+        { !isFetching && dataFiltered.map((card) => {
+          return <Card 
+            title={card.title}
+            cape={card.cape}
+            episode={card.episode}
+            path={card.path}
+            key={card.path}
+          />
+        }) }
       </ul>
     </section>
   )
